c0761ffcfd7aa1ff83d6d3dc50edd074d189a553
Johann Gawe <j.gawe@lingner.com>
2024-01-23 17:42:35 +0100

    set max-width to content areas like main, header, footer

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,22 +1,16 @@
 <script setup>
-import { useCounterStore } from '@/stores/counter.js';
 import lidlLogo from '@/assets/logo.svg';
-import { SimpleCart } from '@iconoir/vue';
-
-const counter = useCounterStore()
+import ClientCart from '@/components/ClientCart.vue'
 </script>
 
 <template>
   <header class="header">
-    <div class="header__logo">
-      <img :src="lidlLogo" alt="Lidl Logo Deutschland">
+    <div class="header__inner">
+      <div class="header__logo">
+        <img :src="lidlLogo" alt="Lidl Logo Deutschland">
+      </div>
+      <ClientCart />
     </div>
-    <button class="header__cart">
-      <SimpleCart width="32" height="32" />
-      <span class="header__cart-count" :class="counter.count > 0 ? 'show' : ''">
-        {{ counter.count }}
-      </span>
-    </button>
   </header>
 </template>
 
@@ -27,51 +21,23 @@ const counter = useCounterStore()
   left: 0;
   width: 100%;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
   padding: 12px 16px;
   background-color: var(--clr-white);
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
 
+  z-index: 999;
+
+  &__inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    max-width: var(--size-max-content);
+    margin: 0 auto;
+  }
+
   &__logo {
     display: flex;
-  }
-
-  &__cart {
-    position: relative;
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-  }
-
-  &__cart-count {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 24px;
-    height: 24px;
-    padding: 3px;
-
-    color: var(--clr-white);
-    background-color: var(--clr-brand-blue);
-    border-radius: 50%;
-    border: 2px solid var(--clr-white);
-
-    font-size: 12px;
-    text-align: center;
-    font-weight: bold;
-
-    transform: scale(0.5);
-    opacity: 0;
-    transition: transform 130ms ease-in-out, opacity 130ms ease-in-out;
-
-    &.show {
-      display: block;
-      opacity: 1;
-      transform: scale(1);
-    }
   }
 }
 </style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,11 +1,22 @@
 <script setup>
-import PageHeader from '@/components/PageHeader.vue'
+import PageHeader from '@/components/PageHeader.vue';
+import PageFooter from '@/components/PageFooter.vue';
+import CartModal from '@/components/CartModal.vue'
 </script>
+
 <template>
   <PageHeader />
   <main>
     <slot />
   </main>
-  <footer>
-  </footer>
+  <PageFooter />
+  <CartModal />
 </template>
+
+<style scoped lang="scss">
+main {
+  max-width:  var(--size-max-content);
+  margin: 0 auto;
+  box-shadow: 0 2px 15px 4px rgba(0, 0, 0, 0.15);
+}
+</style>
