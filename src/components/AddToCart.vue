c6f5793245c5fa217c13b41ae271c05bddd0e591
Johann Gawe <j.gawe@lingner.com>
2024-01-23 17:40:12 +0100

    added add-to-card-button component

---
+++ src/components/AddToCart.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useCartStore } from '@/stores/cartStore.js'
+
+const cart = useCartStore()
+
+const props = defineProps({
+  id: {
+    required: true,
+    type: Number
+  },
+  title: {
+    required: true,
+    type: String
+  }
+})
+</script>
+
+<template>
+  <button @click="cart.addItem(id)" class="btn btn--primary" :title="title">
+    <span>
+      {{ title }}
+    </span>
+    <slot />
+  </button>
+</template>
+
+<style lang="scss">
+.btn {
+  &:active {
+
+  }
+}
+</style>
