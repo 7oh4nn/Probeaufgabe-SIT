4615aee086f496b8bb5025ec6dcb81ae6d887b97
Johann Gawe <j.gawe@lingner.com>
2024-01-23 17:41:26 +0100

    added footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__inner">
+    <a href="/">Impressum</a>
+    <a href="/">AGB</a>
+    <a href="/">Datenschutzbestimmungen</a>
+    <a href="/">Gesetzliche Zusatzinformationen</a>
+    <a href="/">Compliance</a>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+  width: 100%;
+  background-color: var(--clr-grey-600);
+  padding: 18px 0;
+
+  &__inner {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    max-width: var(--size-max-content);
+    margin: 0 auto;
+  }
+
+  a {
+    color: var(--clr-white);
+    text-decoration: none;
+    padding: 16px 8px;
+  }
+}
+</style>
