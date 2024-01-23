ef67d27d1a82d37be4afc85be60dc896a62eaa92
Johann Gawe <j.gawe@lingner.com>
2024-01-23 17:41:48 +0100

    added tab-accordion component

--- src/components/TabModule.vue
+++ src/components/TabModule.vue
@@ -1,115 +1,47 @@
 <script setup>
-import {ref} from 'vue';
-import {NavArrowRight} from "@iconoir/vue";
+import { ref, provide, computed, onMounted } from 'vue'
 
-const tabs = [
-  {
-    id: 1,
-    title: 'Tab 1',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto cumque, dolorum expedita facilis illum impedit ipsam iste perferendis praesentium.'
-  },
-  {
-    id: 2,
-    title: 'Tab 2',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto cumque, dolorum expedita facilis illum impedit ipsam iste perferendis praesentium.'
-  },
-  {
-    id: 3,
-    title: 'Tab 3',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto cumque, dolorum expedita facilis illum impedit ipsam iste perferendis praesentium.'
-  }
-]
+const expanded = ref(null)
+const firstItemId = ref(null);
 
-const openTab = ref(0)
+provide('accordion-register', () => {
+  const id = Symbol()
 
-function toggleContent(event, index) {
-  console.log(index)
-}
-</script>
-
-<template>
-  <ul class="accordion">
-    <li v-for="(tab, index) in tabs" class="accordion__item">
-      <button :id="`accordionitem-${index}`"
-              class="accordion__toggle [ js-toggle ]"
-              @click="openContent(index)"
-              :data-tab-number="index"
-              :aria-expanded="index === openTab ? 'true' : 'false'"
-              :aria-controls="`content-${index}`">
-        <NavArrowRight/>
-        <span role="heading" aria-level="3">{{tab.title}}</span>
-      </button>
-      <div :id="`content-${index}`"
-           class="accordion__content [ js-content ]"
-           data-tab-number="1"
-           role="region"
-           :aria-labelledby="`accordionitem-${index}`"
-           :aria-hidden="index === openTab ? 'false' : 'true'">
-        <div class="accordion__content-inner [ js-content-height ]">
-          <h4 class="h5">Here could be any content</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto cumque, dolorum expedita
-            facilis illum impedit ipsam iste perferendis praesentium.</p>
-        </div>
-      </div>
-    </li>
-  </ul>
-</template>
-
-<style scoped lang="scss">
-.accordion {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-
-  &__item {
-
-  }
-
-  &__toggle {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    padding: 12px 16px;
-    width: 100%;
-    text-align: left;
-    border-radius: 0;
-    border: none;
-    border-bottom: 1px solid var(--clr-grey-200);
-    background-color: var(--clr-grey-100);
-
-    span, svg {
-      pointer-events: none;
+  onMounted(() => {
+    // Set the first item as expanded initially
+    if (!firstItemId.value) {
+      firstItemId.value = id;
+      expanded.value = id;
     }
+  });
 
-    svg {
-      transition: transform 200ms ease-in-out;
-    }
+  return {
+    expanded: computed(() => expanded.value === id),
 
-    &[aria-expanded="true"] {
-      background-color: var(--clr-white);
+    toggle() {
+      expanded.value = expanded.value === id ? expanded.value : id
+    },
 
-      svg {
-        transform: rotate(90deg);
+    unregister() {
+      if (expanded.value === id) {
+        expanded.value = null
       }
     }
   }
-  &__content {
-    height: auto;
-    // open / close of accordion by changing max-height
-    max-height: 0;
+})
+</script>
 
-    overflow: hidden;
+<template>
+  <div class="tab-module">
+    <slot />
+  </div>
+</template>
 
-    /* Content Inner - max-height of content-area is set via outerHeight of this element */
-    &-inner {
-
-    }
-
-    &[aria-hidden="false"] {
-      // use css variables set by JS to open content on toggle to its max-height
-      max-height: var(--content-toggle-item-max-height);
-    }
-  }
+<style scoped lang="scss">
+.tab-module {
+  position: relative;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/TabModulePanel.vue
@@ -0,0 +1,173 @@
+<script setup>
+import { onMounted, ref, inject, onUnmounted } from 'vue'
+import { NavArrowRight } from '@iconoir/vue'
+
+const props = defineProps({
+  title: {
+    required: true,
+    type: String
+  },
+  index: {
+    required: true,
+    type: Number
+  }
+})
+
+const openTabMaxHeight = ref(0)
+const accordionInner = ref([])
+
+const register = inject('accordion-register')
+const { expanded, toggle, unregister } = register()
+
+function setCurrentMaxHeight(index) {
+  openTabMaxHeight.value = accordionInner.value.clientHeight
+}
+
+function handleClick() {
+  toggle()
+  setCurrentMaxHeight(props.index)
+}
+
+onMounted(() => {
+  openTabMaxHeight.value = 10000 // some random big number as first value
+})
+
+onUnmounted(unregister)
+</script>
+
+<template>
+  <button :id="`tabModuleItem-${index}`"
+          class="tab-module__toggle"
+          @click="handleClick"
+          :aria-expanded="expanded ? 'true' : 'false'"
+          :aria-controls="`tabModuleContent-${index}`">
+    <NavArrowRight />
+    <span role="heading" aria-level="3">
+          {{ title }}
+        </span>
+  </button>
+  <div :id="`tabModuleContent-${index}`"
+       class="tab-module__content"
+       role="region"
+       :style="expanded ? `max-height: ${openTabMaxHeight}px` : ''"
+       :aria-labelledby="`accordionitem-${index}`"
+       :aria-hidden="expanded ? 'false' : 'true'">
+    <div class="accordion__content-inner" ref="accordionInner">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.tab-module {
+  // no code here
+  &__toggle {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+
+    padding: 12px 16px;
+
+    font-size: 1.2rem;
+    text-align: left;
+    border-radius: 0;
+    border: none;
+    border-bottom: 1px solid var(--clr-grey-200);
+    background-color: var(--clr-grey-100);
+    cursor: pointer;
+
+    @media screen and (min-width: 768px) {
+      padding: 16px 24px;
+      font-size: 1.6rem;
+    }
+
+    @media screen and (min-width: 1280px) {
+      position: absolute;
+      bottom: 347px;
+      left: 16px;
+      width: max-content;
+      font-size: 1.6rem;
+      border-radius: 6px 6px 0 0;
+      border: none;
+      z-index: 9;
+
+      svg {
+        display: none;
+      }
+    }
+
+    &:hover, &:focus-visible {
+      color: var(--clr-brand-blue);
+    }
+
+    span, svg {
+      pointer-events: none;
+    }
+
+    svg {
+      width: 18px;
+      height: 18px;
+      margin-right: 8px;
+      transition: transform 200ms ease-in-out;
+
+      @media screen and (min-width: 768px) {
+        width: 32px;
+        height: 32px;
+      }
+    }
+
+    &[aria-expanded="true"] {
+      background-color: var(--clr-white);
+      color: var(--clr-brand-blue);
+      cursor: default;
+
+      svg {
+        transform: rotate(90deg);
+      }
+    }
+  }
+
+  &__content {
+    height: auto;
+    // open / close of accordion by changing max-height
+    max-height: 0;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+    overflow: hidden;
+    visibility: hidden;
+    transition: max-height 300ms ease-in-out, visibility 0ms ease 300ms;
+
+    @media screen and (min-width: 1280px) {
+      max-height: 100% !important;
+      opacity: 0.4;
+      transition: opacity 200ms ease-in-out, visibility 0ms ease 200ms;
+    }
+
+    &:not(:first-of-type) {
+      @media screen and (min-width: 1280px) {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+      }
+    }
+
+    /* Content Inner - max-height of content-area is set via outerHeight of this element */
+    &-inner {
+      border-bottom: 4px solid var(--clr-grey-200);
+    }
+
+    &[aria-hidden="false"] {
+      // use css variables set by JS to open content on toggle to its max-height
+      max-height: var(--content-toggle-item-max-height);
+      visibility: visible;
+      transition: max-height 300ms ease-in-out, visibility 0ms ease;
+
+      @media screen and (min-width: 1280px) {
+        opacity: 1;
+        transition: opacity 200ms ease-in-out 280ms;
+      }
+    }
+  }
+}
+</style>
